ebc356a8af5303efddf610a02907663240972023
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-22 12:33:25 +0100

    Only allowing logged in users to create restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -23,6 +23,7 @@ feature 'restaurants' do
 
   context 'creating restaurants' do
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      sign_up_user
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
@@ -31,8 +32,15 @@ feature 'restaurants' do
       expect(current_path).to eq '/restaurants'
     end
 
+    scenario 'a user has to be logged in' do
+      visit '/'
+      click_link 'Add a restaurant'
+      expect(current_path).not_to eq '/restaurants/new'
+    end
+
     context 'an invalid restaurant' do
       it 'does not let you submit a name that is too short' do
+        sign_up_user
         visit '/restaurants'
         click_link 'Add a restaurant'
         fill_in 'Name', with: 'kf'
@@ -60,6 +68,7 @@ feature 'restaurants' do
     before {Restaurant.create name: 'KFC'}
 
     scenario 'let a user edit a restaurant' do
+       sign_up_user
        visit '/restaurants'
        click_link 'Edit KFC'
        fill_in 'Name', with: 'Kentucky Fried Chicken'
@@ -74,11 +83,21 @@ feature 'restaurants' do
     before {Restaurant.create name: 'KFC'}
 
     scenario 'removes a restaurant when a user clicks a delete link' do
+      sign_up_user
       visit '/restaurants'
       click_link 'Delete KFC'
       expect(page).not_to have_content 'KFC'
       expect(page).to have_content 'Restaurant deleted successfully'
     end
+  end
+
+  def sign_up_user
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
   end
 
 end
